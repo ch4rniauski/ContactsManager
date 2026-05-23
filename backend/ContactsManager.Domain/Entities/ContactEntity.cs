0657fab701367899c84e371c4a6e8db8067e9376
Yauheni Charniauski <chernyavskij.z@mail.ru>
2026-05-23 15:50:07 +0300

    Add project structure

---
+++ backend/ContactsManager.Domain/Entities/ContactEntity.cs
@@ -0,0 +1,12 @@
+namespace ContactsManager.Domain.Entities;
+
+public class ContactEntity
+{
+    public string Name { get; set; } = string.Empty;
+
+    public string MobilePhone { get; set; } = string.Empty;
+
+    public string JobTitle { get; set; } = string.Empty;
+
+    public DateOnly BirthDate { get; set; }
+}
